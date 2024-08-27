431635ac5e0eb9d5a963d78c69364263b1209343
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-28 00:06:35 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
 
-scp -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/ -P $REMOTE_PORT -i ~/.ssh/id_rsa
+scp -P $REMOTE_PORT -i ~/.ssh/id_rsa -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/
 
 ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT -i ~/.ssh/id_rsa << 'EOF'
   cd /data/smart/
