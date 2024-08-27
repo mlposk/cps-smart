385df58ab78041900ad64b4a0a0546ffb219e104
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-27 23:35:40 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
 
-scp -r * $REMOTE_USER@$REMOTE_HOST:$WORKDIR
+scp -r * $REMOTE_USER@$REMOTE_HOST:$WORKDIR -P $REMOTE_PORT
 
-ssh $REMOTE_USER@$REMOTE_HOST << 'EOF'
+ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT << 'EOF'
   cd $WORKDIR
   docker-compose down
   docker-compose up -d
