99e9a7cc32b709d9babbb745815ce54c56fe8084
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-28 00:35:57 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-scp -P $REMOTE_PORT -r * $REMOTE_USER@$REMOTE_HOST:$WORKDIR
-
 ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT << 'EOF'
+  touch "file_$(date +%s).txt"
   cd $WORKDIR
   docker-compose down
   docker-compose up -d
