3aa87fe9b334f4cf9a284f088af88ed5edb80266
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-28 00:05:28 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
 
-scp -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/ -P $REMOTE_PORT
+scp -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/ -P $REMOTE_PORT -i ~/.ssh/id_rsa
 
-ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT << 'EOF'
+ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT -i ~/.ssh/id_rsa << 'EOF'
   cd /data/smart/
   docker-compose down
   docker-compose up -d
