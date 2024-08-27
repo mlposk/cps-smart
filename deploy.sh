bcc27d4d72c220b913e165581df67dd592ac1077
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-28 00:09:27 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
+eval $(ssh-agent)
+ssh-add - <<< "${{ secrets.SSH_PRIVATE_KEY }}"
 
 scp -P $REMOTE_PORT -i ~/.ssh/id_rsa -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/
 
