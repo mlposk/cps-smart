41987695a295bf8e8c83c8ec033fc57d6f171d6f
Alexander Molokotin <36039950+mlposk@users.noreply.github.com>
2024-08-28 00:24:16 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
+scp -P $REMOTE_PORT -r * $REMOTE_USER@$REMOTE_HOST:$WORKDIR
 
-mkdir -p ~/.ssh/ && touch ~/.ssh/known_hosts
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-keyscan -H $REMOTE_HOST >> ~/.ssh/known_hosts
-eval $(ssh-agent)
-ssh-add - <<< "$SSH_PRIVATE_KEY"
-
-scp -P $REMOTE_PORT -i ~/.ssh/id_rsa -r * $REMOTE_USER@$REMOTE_HOST:/data/smart/
-
-ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT -i ~/.ssh/id_rsa << 'EOF'
-  cd /data/smart/
+ssh $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT << 'EOF'
+  cd $WORKDIR
   docker-compose down
   docker-compose up -d
 EOF
